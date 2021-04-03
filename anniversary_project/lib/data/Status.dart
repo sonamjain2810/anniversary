0b559d7896856f8ad2e92fffc023495b4b79e571
Sonam Jain <sonamjain2810@yahoo.com>
2021-04-03 16:46:22 +0530

    First Version

---
+++ anniversary_project/lib/data/Status.dart
@@ -0,0 +1,167 @@
+class Status {
+  Status._();
+
+  static const status_data = [
+    // Status
+
+   "Thank God, because on this day he has sent me someone special like you to my life who likes quarrel over me. Happy Wedding Anniversary."
+  ,
+   "Life wouldn’t be so wonderful If you weren’t there to hold me up every time I fall. I feel very happy to be with you every moment. Happy anniversary!"
+  ,
+   "Sweetheart, I love you terribly. With you, all the experiences are beautiful for me. I feel peach when you are next to me. Wish you a Happy Wedding Anniversary."
+  ,
+   "On this very special day, I want you to know that you are everything that I have always wanted. Thanks for being with me all these times. I love you a lot!"
+  ,
+   "It doesn’t matter how many times I have told you that I love you, actually, it never gets old to say that I love you. Happy anniversary my sweetheart."
+  ,
+   "And I’d choose you; in a hundred lifetimes, in a hundred worlds, in any versions of reality, I’d find you and I’d choose you. Happy Anniversary!"
+  ,
+   "Anniversary is the day when everyone remembers about the memories and moments of their relation. So, today is your day to do this. Happy Anniversary."
+  ,
+   "May your bonds of marriage strengthen, Holding strong amidst all fears. And may our friendship stay untarnished, through the wearing of the years."
+  ,
+   "May this day bring a lot of happiness and cheers in your life and the upcoming year. May you never see any problem. I want to see you happy."
+  ,
+   "On this special day, I wish that your love grows stronger and blooms year by year!"
+  ,
+   "Your relation is the dearest relation in the world. This is a relation of body and soul which allows you both to fall in love with each other."
+  ,
+   "Why are you two still together up to now? Maybe God thinks that you are the perfect match. Happy anniversary!!"
+  ,
+   "You two were made for each other, and nothing else matters. Wishing you the best day ever!!"
+  ,
+   "Today you have passed another year of life. It means that you are doing well for the journey of your life."
+  ,
+   "The best thing that ever happened to me is you. You complete me. Happy anniversary my dear husband!"
+  ,
+   "You are my love, my friend, my knight in shining armor. You are my soul mate. Happy anniversary to my husband!"
+  ,
+   "Happy anniversary to the sweetest man I know. Thank you for everything that you do. I love you with all my heart."
+  ,
+   "You are the meaning of my life. Happy anniversary my darling husband!"
+  ,
+   "I have found my soul mate, my hero, my best friend in you. You are my perfect husband. Happy anniversary to my husband!"
+  ,
+   "It’s easy to fall in love, but it’s much harder to stay in love with the same person for the rest of one’s life."
+  ,
+   "Congratulations for another wonderful year of falling in love with each other. Happy Anniversary!"
+  ,
+   "Even though another year has passed, the love you share continues to last. Sending all my best wishes that you will continue to be blessed in love and happiness."
+  ,
+   "The harsh realities of everyday life, bitterness of failure at work and regrets of the past – all these things become bearable just because I have a husband like you. Happy anniversary."
+  ,
+   "Happy anniversary my dear. I wish our love grow higher than the mountain, wider than the sky, brighter than the moon and warmer than the sun."
+  ,
+   "Since I met you, my life has never been the same. Thanks for making it better and happier. I love you and Happy Anniversary, wifey!"
+  ,
+   "I just have one thing to say on our anniversary, “I love you”."
+  ,
+   "Honey, it doesn’t matter how many hardships I have been through. As long as you are by my side, I can manage it. Happy anniversary my dear!"
+  ,
+   "Kiss of love and hug of care, My heart my soul with you I share! Happy wedding anniversary!"
+  ,
+   "I am so lucky to have you as my wife and this is the best day of my life. Happy anniversary to you my dear."
+  ,
+   "Thank you for being my everything and so much more. Happy anniversary."
+  ,
+   "This day reminds me that God really loves me for He gave the most wonderful person and chose her to be my lifetime partner I will always be grateful. Happy Anniversary to you, sweetheart!"
+  ,
+   "The best thing that ever happened to me is you. You complete me. Happy anniversary my dear wife!"
+  ,
+   "Wishing you all the happiness and love in the world and congratulations on your anniversary."
+  ,
+   "Looking at divorced couples make me feel that marriages are ephemeral. But you both have proved that marriages are truly eternal. Happy anniversary."
+  ,
+   "Sending you warm wishes on your anniversary; may you continue to grow older and happier together."
+  ,
+   "Some of the best memories of a couple are not just the magic of their first few kisses but the magic they create every time they kiss for the rest of their lives. Happy anniversary."
+  ,
+   "May you continue to be a wonderful husband and wife to each other. Wishing you nothing but happiness, love, and joy in the years ahead."
+  ,
+   "It feels like as the years go by, each one gets a little bit better. I love being married to you and I wish you the happiest of anniversaries."
+  ,
+   "Not a long journey, your marriage is an adventure. Not a love story, your marriage is an epic tale of romance. Not a happy ending, your marriage is a blissful loop of sweet memories. Happy wedding anniversary."
+  ,
+   "Sending you both loving wishes on your anniversary, may it be something so very special."
+  ,
+   "Even from the start, I knew you were the perfect one for me. Wishing us a very wonderful happy anniversary."
+  ,
+   "You are the sweetest, kindest and humblest person I’ve ever known\nI think I am the luckiest girl in the world coz that person is my husband\nHappy anniversary, hubby!"
+  ,
+   "Wishing you a very happy anniversary and that the future years are just as great as the years of marriage that we’ve had."
+  ,
+   "No one and nothing in this world is perfect, but the two of you are as close as it gets. Happy anniversary."
+  ,
+   "A whole year next to you my love has been a blessing, I feel like every day I love you a little more. Happy Anniversary!"
+  ,
+   "Since you are beside me I have always been very happy. I am grateful to share this love with a person like you. I love you. Happy Anniversary!"
+  ,
+   "I know that we have both changed over the years, but through that change we have grown closer together. I love you so much! Happy Anniversary Sweetheart."
+  ,
+   "Happy anniversary to my one and only. I can never find the words to say how much I love you so I got you a little something to give you a clue – a gorgeous gift for a gorgeous girl."
+  ,
+   "It doesn’t matter how the past has been… I promise, our future will be the best anyone has ever seen. Happy anniversary."
+  ,
+   "You always make me smile and for that I’ll always be thankful. You’re truly the best woman that a man could ever ask for. I’m truly lucky to have you as my girlfriend. Happy anniversary!"
+  ,
+   "Words simply cannot tell how much I love you. There aren’t enough words in the world to express my feelings for you, so I will just say: I love you."
+  ,
+   "No matter where I go in life, I’m happy knowing that I have a wonderful woman like you by my side. No other woman could ever make me feel the way that you do. Happy Anniversary."
+  ,
+   "I had been eagerly waiting for our togetherness to complete a year. And today it finally has arrived! Happy Anniversary, Babe!"
+  ,
+   "Our love has finally reached it’s goal by successfully accomplishing a year of togetherness. We will celebrate our anniversary with joy while cherishing our special moments."
+  ,
+   "Your every kiss & every hug tells me that you have kept the promise you made on this day! Happy anniversary! We’ll never let go of each other!"
+  ,
+   "You are my true love of life which I found on this day, I can’t wait to give you some special on this happy anniversary of our blessed relationship, loves you lot honey!"
+  ,
+   "I am a blessed girlfriend indeed! It’s a shout I’d make to let the world know how much I love you. May our love grow sturdy each day. Happy Anniversary."
+  ,
+   "I will always remember the moment we became lovers. I can’t imagine my life without you since then. Thank you for the love and caring. I love you!"
+  ,
+   "For the man that steals my heart, The man that made my heart flutters, The man that sweeps me off my feet and put butterfly in my stomach when he’s around, Happy Anniversary sweetheart!"
+  ,
+   "As we complete another year of togetherness, I pray to the Almighty that he always keeps us bonded like this and make our love stronger."
+  ,
+   "On our anniversary today, I want you to always keep me protected with the shield of your love by embracing me in your arms."
+  ,
+   "♥ Doesn’t matter what comes on my way, I will walk with you till my end breath. Happy Anniversary my sweetheart. I promise that my love for you never gonna die."
+  ,
+   "♥ Marrying you was the best thing I’ve ever done in my life. Thanks for being the perfect partner! Happy first anniversary."
+  ,
+   "♥ Being married to the best wife in the world makes me want to be the best husband in the world to you. We have been blessed with a wonderful marriage because you make it so easy, and you have been nothing but wonderful. Happy first anniversary my love."
+  ,
+   "♥ Before I met you, I never knew what it was like to be able to look at someone and smile for no reason. Happy 1st anniversary honey."
+  ,
+   "♥ I am so lucky that you are mine. Thank you for loving and taking care of me like you do. Happy Anniversary honey, love you."
+  ,
+   "♥ The more time that we spend together, the more I discover new things to love about you. I don’t think I will ever stop being in awe of you. Even after one year together, I get butterflies every time I see you."
+  ,
+   "♥ The more time I spend with you, the deeper I fall in love. May the rest of our lives always be like our first anniversary."
+  ,
+   "♥ Happy first anniversary to my first and last love who has stolen my heart, but given me beautiful her soul in return."
+  ,
+   "♥ Looking back I’m glad I chose you and you chose me. Looking ahead I’m grateful I’ll be spending my future with you. Happy first anniversary my sweetheart."
+  ,
+   "All these years, you have been amazing. As a dad to our kids and as my lover, I have been happy in your arms and I could see that we’ll be happier for the rest of our days. Thank you for everything. Happy anniversary, my darling."
+  ,
+   "Since I met you, my life has never been the same. Thanks for making it better and happier. I love you and Happy Anniversary, hubby! I thank God for giving me the best husband!"
+  ,
+   "I am so lucky to have you as my husband and this is my best day in my life. Happy anniversary to you my dear."
+  ,
+   "The very first moment I laid my eyes on you, I knew our hearts were meant to be. You are my courage, my angel, my soldier. You saved me. I love you."
+  ,
+   "You are truly a blessing from God. Thank you for being my partner, husband, lover and friend. Happy anniversary!"
+  ,
+   "You’re the cheese to my macaroni.\nNo one else would understand our relationship, and no one else in this world would understand me the way you do. Happy anniversary!"
+  ,
+   "Thank you for being there, especially when times get difficult. I love you and always will."
+  ,
+   "With you, I became a better person. Without you, my life will never be complete. Thank you for choosing me as your better half. Happy anniversary!"
+  
+
+
+  
+  ];
+}
